7c22b3e03ad23e85106610f3a705647e73a6d8cc
Jurgen Neziri <j.neziri.cal@gmail.com>
2022-10-27 17:41:32 +0200

    1.0

--- src/main/java/co/develhope/Swagger_01/controllers/NameController.java
+++ src/main/java/co/develhope/Swagger_01/controllers/NameController.java
@@ -1,19 +1,20 @@
 package co.develhope.Swagger_01.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/name")
 public class NameController {
-    @RestController
-    @RequestMapping("/name")
-    public class nameController {
 
-        @GetMapping(value = "/name/{name}")
-        public String  name(@ApiParam(value = "the id of the user")@PathVariable()  String name) {
-            return "Welcome "+name;
-        }
+
+    @GetMapping(value = "/{name}")
+    public String  welcomeWithNamePath(@ApiParam(value = "the name of the user")@PathVariable()  String name) {
+        return "Welcome "+name;
     }
 }
+
